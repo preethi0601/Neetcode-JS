bda20e972028226ee004ab32c5d7a75b48c64954
Prithi <48240941+preethi0601@users.noreply.github.com>
2024-05-05 12:31:03 +0200

    Time: 71 ms (6.47%) | Memory: 49.2 MB (78.09%) - LeetSync

--- 2749-promise-time-limit/promise-time-limit.js
+++ 2749-promise-time-limit/promise-time-limit.js
@@ -7,8 +7,8 @@ var timeLimit = function(fn, t) {
     
     return async function(...args) {
         return new Promise((resolve, reject) => {
-            setTimeout(() => reject("Time Limit Exceeded"), t); 
-            fn(...args).then(resolve, reject);
+            let timer  = setTimeout(() => reject("Time Limit Exceeded"), t); 
+            fn(...args).then(resolve, reject).finally(() => clearTimeout(timer));
         })
     }
 };
